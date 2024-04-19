43b8359625fe3adbaf27025fe0700e49156d38d6
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-04-19 17:14:37 +0000

    Trying to rollback changes made.

--- session_state.py
+++ session_state.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
-class _SessionState:
+import streamlit as st
+
+class _sessionstate:
     def __init__(self):
         self.user_authenticated = False
 
 def get(**kwargs):
     """Gets a sessionstate object for the current session."""
     if not hasattr(st, '_session_state'):
-        st._session_state = _SessionState()
+        st._session_state = _sessionstate()
     for key, value in kwargs.items():
         if not hasattr(st._session_state, key):
             setattr(st._session_state, key, value)
