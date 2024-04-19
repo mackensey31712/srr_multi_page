d5a299eae71577073ec448aabe1eb4abfb59ccd0
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-04-19 17:05:40 +0000

    made changes in the session_state.py file by changing sessionstate to _SessionState()

--- session_state.py
+++ session_state.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-class _sessionstate:
+class _SessionState:
     def __init__(self):
         self.user_authenticated = False
 
 def get(**kwargs):
     """Gets a sessionstate object for the current session."""
     if not hasattr(st, '_session_state'):
-        st._session_state = _sessionstate()
+        st._session_state = _SessionState()
     for key, value in kwargs.items():
         if not hasattr(st._session_state, key):
             setattr(st._session_state, key, value)
