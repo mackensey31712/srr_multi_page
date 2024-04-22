489a57e2b5d40236b9f47dcae6e66fca2b952b33
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-04-22 17:25:35 +0000

    Edited the Homepage Welcome messages.

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -28,11 +28,11 @@ def main():
                 st.error("😕Incorrect username or password")
     else:  # Show Homepage if authenticated
         st.title("Homepage")
-        st.markdown(f"Welcome, ***{st.session_state.username}***! to our Streamlit Multipage App")
+        st.markdown(f"Welcome, ***{st.session_state.username}***! to our Five9 SRR Management Multipage App")
         
 
         markdown_text = """
-        Our app is designed to provide a comprehensive solution for managing and analyzing srr data. Whether you're a supervisor handling your team's day-to-day operations, a manager seeking insights and oversight, or an analyst diving deep into the data, we have you covered with our streamlined interface and powerful features.
+        Our app is designed to provide a comprehensive solution for managing and analyzing SRR data. Whether you're a supervisor handling your team's day-to-day operations, a manager seeking insights and oversight, or an analyst diving deep into the data, we have you covered with our streamlined interface and powerful features.
 
         Here's a brief overview of what you can expect from each page:
 
@@ -40,15 +40,15 @@ def main():
         This is your gateway to the app's functionality. From here, you can navigate to the different sections and explore the tools tailored to your needs.
 
         2. **Agent View**:
-        Dive into the agent srr dashboard, where agents can efficiently track and manage service requests. Gain real-time insights into task status, pending requests, and overall performance to optimize workflow and enhance customer satisfaction.
+        Dive into the agent SRR dashboard, where agents can efficiently track and manage service requests. Gain real-time insights into task status, pending requests, and overall performance to optimize workflow and enhance customer satisfaction.
 
         3. **Management View**:
         Building upon the agent view, the management view provides additional charts and metrics tailored for managerial needs. Gain deeper insights into team performance, resource allocation, and trends over time to make informed decisions and drive operational excellence.
 
         4. **SRR Analytics Tool**:
-        This powerful tool empowers management to explore, transform, and visualize srr data with ease. Utilizing a simple drag-and-drop dashboard interface, users can uncover patterns, identify outliers, and extract valuable insights. Additionally, this page offers basic exploratory data analysis (EDA) to kickstart your data exploration journey.
+        This powerful tool empowers management to explore, transform, and visualize srr data with ease. Utilizing a simple drag-and-drop dashboard interface, users can uncover patterns, identify outliers, and extract valuable insights. Additionally, this page offers basic Exploratory Data Analysis (EDA) to kickstart your data exploration journey.
 
-        Whether you're on the front lines resolving service requests or at the helm steering strategic decisions, our app is here to support you every step of the way. Get ready to unlock the full potential of your srr data and elevate your operational efficiency with our Streamlit multipage app!
+        Whether you're on the front lines resolving service requests or at the helm steering strategic decisions, our app is here to support you every step of the way. Get ready to unlock the full potential of your SRR data and elevate your operational efficiency with our Five9 SRR Management Multipage App!
         """
 
         st.markdown(markdown_text)
