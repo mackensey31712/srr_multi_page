e98ffd903b973e70b44941e730632a70e281f577
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-05-08 08:06:29 +0000

    Dropping "null" rows in "Service" because the dataframe is including the "null" rows in the Gsheet.

--- pages/4_SRR_Analytics_Tool.py
+++ pages/4_SRR_Analytics_Tool.py
@@ -39,6 +39,7 @@ else:
         df.rename(columns={'In process (On It SME)': 'SME (On It)'}, inplace=True)  
         df['TimeTo: On It (Raw)'] = df['TimeTo: On It'].copy()
         df['TimeTo: Attended (Raw)'] = df['TimeTo: Attended'].copy()
+        df.dropna(subset=['Service'], inplace=True)
         return df
 
     # url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSQVnfH-edbXqAXxlCb2FrhxxpsOHJhtqKMYsHWxf5SyLVpAPTSIWQeIGrBAGa16dE4CA59o2wyz59G/pub?gid=0&single=true&output=csv'
